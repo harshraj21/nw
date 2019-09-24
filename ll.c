27a45b4be8ad1261deff25e5aac81c99ca802636
Harsh Raj <harsh.raj_98@protonmail.com>
2019-09-24 08:18:02 +0530

    Casual Updates

---
+++ ll.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int val;
+    struct node *next;
+}*ptr;
+
+struct node *start;
+struct node *end;
+
+void display();
+void create();
+void delete();
+
+void main(){
+    int ch;
+
+    start = NULL;
+    end = NULL;
+
+    for(;;){
+        printf("\nLinked List Generation\n1: Create\n2: Delete\n3: Display\n4: Exit\nEnter Your Choice: ");
+        scanf("%d",&ch);
+        switch (ch)
+        {
+            case 1: create();
+            break;
+
+            case 2: delete();
+            break;
+
+            case 3: display();
+            break;
+
+            case 4: exit(1);
+            break;
+        
+            default: printf("\nInvalid Choice!\n");
+            break;
+        }
+    }
+}
+
+void create(){
+    int val2;
+    printf("\nEnter The Value To Store: ");
+    scanf("%d",&val2);
+    ptr = (struct node*)malloc(sizeof(struct node));
+    if(start == NULL){
+        if(ptr == NULL){
+            printf("\nWarning: Computer Memory Full!\n");
+            return;
+        }else{
+            ptr->val = val2;
+            ptr->next = NULL;
+            start = ptr;
+            printf("\nInitial List Sucessfully Generated and value stored!\n\n");
+        } 
+    }
+    else{
+        ptr->val = val2;
+        ptr->next = start;
+        start = ptr;
+        printf("\nLinked List Sucessfully Generated and value stored!\n\n");
+    }  
+}
+
+void display(){
+    int i;
+    if(start == NULL){
+        printf("\nWarning: List is Empty!\n");
+    }
+    else{
+        i=0;
+        printf("\nPrinting the stored values\n\n");
+        while(ptr->next!=NULL){
+            printf("%d: %d\n",++i,(ptr->val));
+            ptr = ptr->next;
+        }
+    }
+}
+
+void delete(){
+
+}

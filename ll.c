c0eec2d91149e79a9556deb4ad963db574fbd32f
Harsh Raj <harsh.raj_98@protonmail.com>
2019-09-25 20:32:40 +0530

    updt

--- ll.c
+++ ll.c
@@ -7,21 +7,26 @@ struct node{
 }*ptr;
 
 struct node *start;
+int count;
 
 void display();
 void create();
 void delete();
+void del_all();
 
 void main(){
     int ch;
-
+    count = 0;
     start = NULL;
 
     for(;;){
-        printf("\nLinked List Generation\n1: Insert one\n2: Delete one\n3: Delete by elem.\n4: Delete by pos.\n5: Display all\n6: Delete all\n7: Insert by position\n8: Insert by value (after)\n9: Insert by value (before)\n0: Exit\nEnter Your Choice: ");
+        printf("\nLinked List Generation\n0: Exit\n1: Insert one\n2: Delete one\n3: Delete by elem.\n4: Delete by pos.\n5: Display all\n6: Delete all\n7: Insert by position\n8: Insert by value (after)\n9: Insert by value (before)\n10: Elements count\nEnter Your Choice: ");
         scanf("%d",&ch);
         switch (ch)
         {
+            case 0: exit(1);
+            break;
+
             case 1: create();
             break;
 
@@ -31,7 +36,10 @@ void main(){
             case 5: display();
             break;
 
-            case 0: exit(1);
+            case 6: del_all();
+            break;
+
+            case 10: printf("\nNo of element present in list is: %d\n",count);
             break;
         
             default: printf("\nInvalid Choice!\n");
@@ -42,6 +50,8 @@ void main(){
 
 void create(){
     int val2;
+    struct node *tmp;
+    tmp = start;
     printf("\nEnter The Value To Store: ");
     scanf("%d",&val2);
     ptr = (struct node*)malloc(sizeof(struct node));
@@ -55,27 +65,34 @@ void create(){
             ptr->next = NULL;
             start = ptr;
             printf("\nInitial List Sucessfully Generated and value stored!\n\n");
+            count++;
         } 
     }
     else{
         ptr->val = val2;
-        ptr->next = start;
-        start = ptr;
+        ptr->next = NULL;
+        while(tmp->next!=NULL){
+            tmp = tmp->next;
+        }
+        tmp->next = ptr;
         printf("\nLinked List Sucessfully Generated and value stored!\n\n");
+        count++;
     }
 }
 
 void display(){
     int i;
+    struct node *tmp;
+    tmp = start;
     if(start == NULL){
         printf("\nWarning: List is Empty!\n");
     }
     else{
         i=0;
         printf("\nPrinting the stored values\n\n");
-        while(ptr!=NULL){
-            printf("%d: %d\n",++i,(ptr->val));
-            ptr = ptr->next;
+        while(tmp!=NULL){
+            printf("%d: %d\n",++i,(tmp->val));
+            tmp = tmp->next;
         }
     }
 }
@@ -89,8 +106,29 @@ void delete(){
         ptr = start;
         ptr = ptr->next;
         tmp = start->val;
+        start->next = NULL;
         free(start);
         start = ptr;
         printf("\n%d Deleted Sucessfully!\n",tmp);
+        count--;
+    }
+}
+
+void del_all(){
+    struct node *tmp;
+    struct node *tmp2;
+    tmp = start;
+    tmp2 = start;
+    if(start == NULL){
+        printf("\nWarning: List is Empty!\n");
+    }
+    else{
+        while(tmp!=NULL){
+            free(tmp2);
+            tmp = tmp->next;
+            tmp2 = tmp;
+        }
+        start = NULL;
+        printf("\nAll Items Deleted Sucessfully!\n");
     }
 }
